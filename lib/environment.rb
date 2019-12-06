08a4fabf9f191e1f9c77d95e6b3b7c4a49e7297e
Nathan Mousa <nathanmousa1@gmail.com>
2019-12-06 00:13:37 +0000

    Refactored CLI + Error Fix

--- lib/environment.rb
+++
@@ -1,9 +0,0 @@
-require 'pry'
-require 'json'
-require 'rest-client'
-require 'artii'
-
-require_relative "./project/version"
-require_relative "./project/cli"
-require_relative "./project/api_service"
-require_relative "./project/movies"
--- lib/project/version.rb
+++
@@ -1,3 +0,0 @@
-module MovieDB
-  VERSION = "0.1.0"
-end
